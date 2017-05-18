9bdef83be16bdbe70213e0661b1604b11c7c32ef
Jental <jentalmeister@gmail.com>
2017-05-18 09:47:37 +0300

    Previews

---
+++ make_previews.sh
@@ -0,0 +1,16 @@
+#!/bin/sh                                                                                                                                                                                       
+
+IMAGES_DIR="../";
+PREVIEWS_DIR="../previews";
+
+for filepath in $IMAGES_DIR/*[.jpg,.png]
+do
+    filename=$(basename $filepath)
+    if [ ! -f $PREVIEWS_DIR/$filename ]
+       then
+           echo $filename;
+           convert $filepath -resize 320x180 $PREVIEWS_DIR/$filename
+           chown jental:server $PREVIEWS_DIR/$filename
+           chmod 770 $PREVIEWS_DIR/$filename
+    fi
+done
